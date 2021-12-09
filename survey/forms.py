bb78b9e449cab30ab67e02b6c81d1d4e88730db6
MicheleMosca <michele.mosca026@gmail.com>
2021-12-09 17:49:51 +0100

    register and login page created with redirect and authentication control applied

--- survey/forms.py
+++ survey/forms.py
@@ -5,4 +5,4 @@ from django.contrib.auth.forms import UserCreationForm
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', "last_name", 'email', 'password1', 'password2']
--- survey/urls.py
+++ survey/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
     path('', views.indexView, name='index'),
     path('register/', views.registerView, name='register'),
+    path('login/', views.loginView, name='login'),
+    path('home/', views.homeView, name='home'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
