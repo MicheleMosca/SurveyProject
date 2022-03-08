d7915bb3e06f474d4ecc3596480714cd729640d5
MicheleMosca <michele.mosca026@gmail.com>
2022-03-08 12:34:26 +0100

    Write first models of the database

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Survey_Collection(models.Model):
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
+
+
+class Survey(models.Model):
+    survey_collection = models.ForeignKey(Survey_Collection, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Image(models.Model):
+    path = models.CharField(max_length=200)
+    transformation = models.CharField(max_length=200)
+    survey_collection = models.ForeignKey(Survey_Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.path
+
+
+class Choice(models.Model):
+    name = models.CharField(max_length=200)
+    survey_collection = models.ForeignKey(Survey_Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Answer(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
