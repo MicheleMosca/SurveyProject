9b4b3b729bb2a931bb77e9c66a90ed7ae62e44ed
MicheleMosca <michele.mosca026@gmail.com>
2022-03-09 13:05:19 +0100

    Write first models of the database

--- survey/views.py
+++ survey/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from .models import Survey, Image, Answer, Choice
+
 
 def indexView(request):
     return render(request, 'survey/index.html')
@@ -51,5 +53,41 @@ def logoutUser(request):
 
 
 @login_required(login_url='survey:login')
+def surveyView(request):
+    if request.method == 'POST':
+        print(request.POST)
+        answer = Answer(user_id=request.user.id, image_id=request.POST.get('img'),
+                        choice_id=request.POST.get('answer'), comment=request.POST.get('comment'))
+        answer.save()
+
+    images = Image.objects.filter(survey_collection_id=request.GET.get('survey_collection_id'))
+    choices = Choice.objects.filter(survey_collection_id=request.GET.get('survey_collection_id'))
+    context = {
+        'images': images,
+        'choices': choices,
+    }
+    return render(request, 'survey/survey.html', context)
+
+
+@login_required(login_url='survey:login')
 def homeView(request):
-    return render(request, 'survey/home.html')
+    survey_list = []
+    try:
+        survey_list = [Survey.objects.get(user_id=request.user.id)]
+    except (KeyError, Survey.DoesNotExist):
+        survey_list = []
+    context = {
+        'survey_list': survey_list,
+    }
+
+    return render(request, 'survey/home.html', context)
+
+
+def resultView(request):
+    user_answers = Answer.objects.filter(user_id=request.user.id)
+    survey_images = Image.objects.filter(survey_collection_id=request.GET.get('survey_collection_id'))
+    context = {
+        'user_answers': user_answers,
+        'survey_images': survey_images,
+    }
+    return render(request, 'survey/result.html', context)
