47ed1fcde21beaee43fd9e49c0d58faf450c7dab
MicheleMosca <michele.mosca026@gmail.com>
2022-06-27 12:31:19 +0200

    Mirror and flip transformation applied

--- survey/templatetags/survey_extras.py
+++ survey/templatetags/survey_extras.py
@@ -1,14 +1,16 @@
 import base64
+import io
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.finders import find
+from PIL import Image, ImageOps
 
 register = template.Library()
 
 
 @login_required(login_url='survey:login')
 @register.simple_tag
-def encode_static(path, encoding='base64', file_type='image'):
+def encode_static(path, encoding='base64', file_type='image', transformation=''):
     """
         a template tag that returns a encoded string representation of a staticfile
         Usage::
@@ -18,8 +20,19 @@ def encode_static(path, encoding='base64', file_type='image'):
     """
     file_path = find(path)
     ext = file_path.split('.')[-1]
-    with open(file_path, 'rb') as f:
-        file_str = base64.b64encode(f.read()).decode('utf-8')
-        f.close()
+    transformation_list = transformation.split(',')
+
+    img = Image.open(file_path)
+
+    for tr in transformation_list:
+        if tr == 'flip':
+            img = ImageOps.flip(img)
+        if tr == 'mirror':
+            img = ImageOps.mirror(img)
+
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+    file_str = base64.b64encode(img_byte_arr).decode('utf-8')
 
     return f"data:{file_type}/{ext};{encoding},{file_str}"
