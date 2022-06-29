f1952748a6cd33341f8708a1031d510c1418beb8
MicheleMosca <michele.mosca026@gmail.com>
2022-06-29 22:03:30 +0200

    Add a new table called Image_Transformation to apply Collection's transformations to users with a random function based on probability

--- survey/scripts/image_collection_loader.py
+++ survey/scripts/image_collection_loader.py
@@ -1,22 +1,27 @@
-from survey.models import Image, Survey_Collection, Image_Collection, User, Survey, Choice
+from survey.models import Image, Survey_Collection, Image_Collection, User, Survey, Choice, Image_Transformation
 import yaml
+import random
 
 # Usage:
 # python manage.py runscript image_collection_loader --script-args survey/static/survey/collection/new_collection.yaml
 
 errorMsg = {
-    1:  "Incorrect parameters! file.yaml is needed",
-    2:  "The new collection doesn't have any choices for the answer!",
-    3:  "The collection is empty!",
-    4:  "Choices need a name!"
+    1: "Incorrect parameters! file.yaml is needed",
+    2: "The new collection doesn't have any choices for the answer!",
+    3: "The collection is empty!",
+    4: "Choices need a name!"
 }
+
+
+def decision(probability):
+    return random.random() < probability
 
 
 def add_images(images, collection_object):
     for img in images:
         path = img['path']
         name = img.get('name', (path.split('/')[-1]).split('.')[0])
-        transformation = img.get('transformation', '')
+        transformations = collection_object.transformations
 
         image_object = Image.objects.get_or_create(
             path=path,
@@ -25,12 +30,32 @@ def add_images(images, collection_object):
         print(f"path: {image_object.path}  name: {image_object.name}")
         image_collection_object = Image_Collection.objects.get_or_create(image_id=image_object.id,
                                                                          survey_collection_id=collection_object.id)
-        image_collection_object[0].transformation = transformation
         image_collection_object[0].save()
         print(f"Image_collection id: {image_collection_object[0].id} "
               f"Image_collection image_id: {image_collection_object[0].image_id} "
-              f"Image_collection survey_collection_id: {image_collection_object[0].survey_collection_id} "
-              f"Image_collection transformation: {image_collection_object[0].transformation}")
+              f"Image_collection survey_collection_id: {image_collection_object[0].survey_collection_id}")
+
+        users_id = [user['user_id'] for user in Survey.objects.filter(
+            survey_collection_id=collection_object.id).values('user_id')]
+        for user_id in users_id:
+            image_transformation = Image_Transformation.objects.update_or_create(
+                user_id=user_id, image_collection_id=image_collection_object[0].id)[0]
+            image_transformation.applied_transformation = ''
+            for transformation in transformations.split(','):
+                probability = float(transformation.split('(')[1].split(')')[0])
+                transformation = transformation.split('(')[0]
+                if decision(probability):
+                    if transformation == 'contrast':
+                        factor = "%.2f" % random.uniform(0.5, 1.5)
+                        transformation += f'({factor})'
+
+                    if image_transformation.applied_transformation == '':
+                        image_transformation.applied_transformation = transformation
+                    else:
+                        image_transformation.applied_transformation += ',' + transformation
+
+            print(f"User_id: {user_id} Applied Trasnformations: {image_transformation.applied_transformation}")
+            image_transformation.save()
 
 
 def add_users(users, collection_object):
@@ -54,6 +79,16 @@ def add_choices(choices, collection_object):
               f"Collection id: {choice_object[0].survey_collection_id} added!")
 
 
+def add_transformations(transformations, collection_object):
+    print(f"Transformations: {transformations}")
+    transformation_field = transformations[0]
+    for transformation in transformations[1:]:
+        transformation_field += ',' + transformation
+
+    collection_object.transformations = transformation_field
+    collection_object.save()
+
+
 def create_or_modify_collections(data):
     collection = data.get('collection')
     if collection is None:
@@ -63,6 +98,7 @@ def create_or_modify_collections(data):
     collection_id = collection.get('id')
     description = collection.get('description', '')
     choices = collection.get('choices')
+    transformations = collection.get('transformations')
 
     if collection_id is not None:
         print(f"Modifico la collection {collection_id}")
@@ -82,6 +118,9 @@ def create_or_modify_collections(data):
         collection_object = Survey_Collection(description=description)
         collection_object.save()
         print(f"Collection id: {collection_object.id}")
+
+    if transformations is not None:
+        add_transformations(transformations, collection_object)
 
     if choices is not None:
         add_choices(choices, collection_object)
