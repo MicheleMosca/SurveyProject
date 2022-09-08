f06620cb44095499de9ede3700160e15918811b2
MicheleMosca <michele.mosca026@gmail.com>
2022-09-08 20:53:03 +0200

    set default url

--- SurveyProject/urls.py
+++ SurveyProject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -21,5 +22,5 @@ urlpatterns = [
     path('survey/', include('survey.urls')),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/survey/')),
+    path('', lambda request: redirect('survey/', permanent=True)),
 ]
